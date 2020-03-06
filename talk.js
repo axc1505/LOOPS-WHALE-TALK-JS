87bb81ca86c1f4ea2e32f576654ee0430e619496
Alexandru-Cristian Virbanescu <49607393+axc1505@users.noreply.github.com>
2020-03-06 19:31:57 +0200

    Create talk.js

---
+++ talk.js
@@ -0,0 +1,25 @@
+let input="Salut. Azi este Vineri.";
+const vowels=['a', 'e', 'i', 'o', 'u'];
+let resultArray=[];
+
+for(let i=0;i<input.length;i++){
+  //console.log(i);
+  for(let j=0;j<=vowels.length;j++){
+   if(input[i]===vowels[j])
+     {
+       if(input[i]==='e'||input[i]==='u')
+         {
+           resultArray.push('ee')
+         }
+       else if(input[i]==='u'){
+         resultArray.push('uu');
+       }
+       else
+         {
+           resultArray.push(input[i]);
+         }
+     }
+  }
+}
+
+console.log(resultArray.join("").toUpperCase());
